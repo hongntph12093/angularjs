3f2d5b3a72d8566523b92ee54d9ca1320cd999ca
hong.nt <hongnguyen.com>
2022-08-08 14:54:21 +0700

    second commit

--- main.js
+++
@@ -1,5 +0,0 @@
-const app = angular.module("myApp", ["ngRoute"]);
-app.config(routes);
-app.controller("productController", productController);
-app.controller("cateController", cateController);
-app.controller("menuController", menuController);
